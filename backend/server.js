2527d290141d1ae109cae8f134544116a4669b18
JoveMoniz <kufongo@gmail.com>
2026-07-09 15:49:21 +0100

    Bump app version marker

--- backend/server.js
+++ backend/server.js
@@ -162,7 +162,7 @@ app.use((err, req, res, next) => {
 // GLOBAL APP VERSION
 // Changes every backend restart
 // ======================================================
-const APP_VERSION = '20260709f';
+const APP_VERSION = '20260709g';
 
 // ======================================================
 // VERSION ENDPOINT
