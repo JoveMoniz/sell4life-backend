b7500f2cf817ae3c9fc09cd1d7537562e7c18c7c
JoveMoniz <kufongo@gmail.com>
2026-09-02 15:47:53 +0100

    Bump APP_VERSION to trigger a redeploy

--- backend/server.js
+++ backend/server.js
@@ -214,7 +214,7 @@ app.use((err, req, res, next) => {
 // GLOBAL APP VERSION
 // Changes every backend restart
 // ======================================================
-const APP_VERSION = '20260902f';
+const APP_VERSION = '20260902g';
 
 // ======================================================
 // VERSION ENDPOINT
